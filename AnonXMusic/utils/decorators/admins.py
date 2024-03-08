b816a777b513425f0eda2751b8e81db52f659c75
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:34:45 +0700

    update AnonXMusic/utils/decorators/admins.py

--- AnonXMusic/utils/decorators/admins.py
+++ AnonXMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from Musikbot import app
+from Musikbot.misc import SUDOERS, db
+from Musikbot.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
