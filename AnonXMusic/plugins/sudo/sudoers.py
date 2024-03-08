9e4636c1b02ea5648c510abc3db6abb0a20fc1eb
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:43:12 +0700

    update AnonXMusic/plugins/sudo/sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from Musikbot import app
+from Musikbot.misc import SUDOERS
+from Musikbot.utils.database import add_sudo, remove_sudo
+from Musikbot.utils.decorators.language import language
+from Musikbot.utils.extraction import extract_user
+from Musikbot.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
