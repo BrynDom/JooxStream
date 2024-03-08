b49efa2a0e65782a59b765be72a220e6e13f17e8
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:39:26 +0700

    update AnonXMusic/plugins/bot/settings.py

--- AnonXMusic/plugins/bot/settings.py
+++ AnonXMusic/plugins/bot/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import (
+from Musikbot import app
+from Musikbot.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from AnonXMusic.utils.database import (
     skip_off,
     skip_on,
 )
-from AnonXMusic.utils.decorators.admins import ActualAdminCB
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.settings import (
+from Musikbot.utils.decorators.admins import ActualAdminCB
+from Musikbot.utils.decorators.language import language, languageCB
+from Musikbot.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from AnonXMusic.utils.inline.start import private_panel
+from Musikbot.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
