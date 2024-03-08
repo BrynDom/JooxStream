3f4716007aef8711947cd2720e7a522728fb9173
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:42:11 +0700

    update AnonXMusic/plugins/sudo/block.py

--- AnonXMusic/plugins/sudo/block.py
+++ AnonXMusic/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_gban_user, remove_gban_user
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
+from Musikbot import app
+from Musikbot.misc import SUDOERS
+from Musikbot.utils.database import add_gban_user, remove_gban_user
+from Musikbot.utils.decorators.language import language
+from Musikbot.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
