47f87e71427b377b1ab116703b444488389e3093
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:44:37 +0700

    update AnonXMusic/plugins/tools/reload.py

--- AnonXMusic/plugins/tools/reload.py
+++ AnonXMusic/plugins/tools/reload.py
@@ -5,12 +5,12 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
-from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
+from Musikbot import app
+from Musikbot.core.call import Anony
+from Musikbot.misc import db
+from Musikbot.utils.database import get_assistant, get_authuser_names, get_cmode
+from Musikbot.utils.decorators import ActualAdminCB, AdminActual, language
+from Musikbot.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
 rel = {}
