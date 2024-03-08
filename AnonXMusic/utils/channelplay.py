9e00f9a18c5b1f55c3d3a128664f72440877587c
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:19:18 +0700

    update AnonXMusic/utils/channelplay.py

--- AnonXMusic/utils/channelplay.py
+++ AnonXMusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from Musikbot import app
+from Musikbot.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
