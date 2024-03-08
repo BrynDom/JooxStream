e48c49e037470a0cd03bb13f7fab83fc7aa3ba66
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:31:21 +0700

    update AnonXMusic/utils/inline/play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from AnonXMusic.utils.formatters import time_to_seconds
+from Musikbot.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
