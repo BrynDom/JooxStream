e557d106e7d5a117c33dcd69d101f35efc25f8c8
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:37:40 +0700

    update AnonXMusic/plugins/admins/seek.py

--- AnonXMusic/plugins/admins/seek.py
+++ AnonXMusic/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from Musikbot import YouTube, app
+from Musikbot.core.call import Anony
+from Musikbot.misc import db
+from Musikbot.utils import AdminRightsCheck, seconds_to_min
+from Musikbot.utils.inline import close_markup
 from config import BANNED_USERS
 
 
