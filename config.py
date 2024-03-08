f76aac45fa8b5bb0cf0c91816d76b667d26d02cf
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 02:01:25 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/Vooyage21/ty.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
