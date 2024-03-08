2cd80138b8437746cd8e33e79903cc567cddaed3
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:38:58 +0700

    update AnonXMusic/plugins/bot/help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
-from AnonXMusic.utils.database import get_lang
-from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
-from AnonXMusic.utils.inline.help import help_back_markup, private_help_panel
+from Musikbot import app
+from Musikbot.utils import help_pannel
+from Musikbot.utils.database import get_lang
+from Musikbot.utils.decorators.language import LanguageStart, languageCB
+from Musikbot.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
