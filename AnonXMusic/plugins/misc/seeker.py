9017c5b82078709c62d1921f56f13ead398bcfd3
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:40:23 +0700

    update AnonXMusic/plugins/misc/seeker.py

--- AnonXMusic/plugins/misc/seeker.py
+++ AnonXMusic/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_active_chats, is_music_playing
+from Musikbot.misc import db
+from Musikbot.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
