697be3bd462cad022c88b53cde2efa9012e3ad03
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:37:54 +0700

    update AnonXMusic/plugins/admins/shuffle.py

--- AnonXMusic/plugins/admins/shuffle.py
+++ AnonXMusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Musikbot import app
+from Musikbot.misc import db
+from Musikbot.utils.decorators import AdminRightsCheck
+from Musikbot.utils.inline import close_markup
 from config import BANNED_USERS
 
 
