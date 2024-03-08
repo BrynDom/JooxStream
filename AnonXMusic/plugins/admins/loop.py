3ec967c1bc212477124c84c6464632e3cd214628
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:36:54 +0700

    update AnonXMusic/plugins/admins/loop.py

--- AnonXMusic/plugins/admins/loop.py
+++ AnonXMusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Musikbot import app
+from Musikbot.utils.database import get_loop, set_loop
+from Musikbot.utils.decorators import AdminRightsCheck
+from Musikbot.utils.inline import close_markup
 from config import BANNED_USERS
 
 
