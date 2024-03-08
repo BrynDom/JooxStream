78ad50564603cd6911d3c17ecfcc1b5126e04a41
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 00:43:54 +0700

    update AnonXMusic/plugins/tools/language.py

--- AnonXMusic/plugins/tools/language.py
+++ AnonXMusic/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_lang, set_lang
-from AnonXMusic.utils.decorators import ActualAdminCB, language, languageCB
+from Musikbot import app
+from Musikbot.utils.database import get_lang, set_lang
+from Musikbot.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
