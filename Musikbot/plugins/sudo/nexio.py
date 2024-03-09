c162be9661aefb1907378c9625c9bb665160deb9
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:38:56 +0700

    update Musikbot/plugins/sudo/nexio.py

--- Musikbot/plugins/sudo/nexio.py
+++ Musikbot/plugins/sudo/nexio.py
@@ -1,10 +1,10 @@
 import requests
 import random
-from DAXXMUSIC import app, userbot
-from DAXXMUSIC.misc import SUDOERS
+from Musikbot import app, userbot
+from Musikbot.misc import SUDOERS
 from pyrogram import * 
 from pyrogram.types import *
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from Musikbot.utils.daxx_ban import admin_filter
 
 
 
