188b6afece9ba8edf73c233ed1e90f42d86df0fb
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:30:08 +0700

    Add files via upload

--- Musikbot/plugins/misc/seeker.py
+++ Musikbot/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from Musikbot.misc import db
-from Musikbot.utils.database import get_active_chats, is_music_playing
+from DAXXMUSIC.misc import db
+from DAXXMUSIC.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
