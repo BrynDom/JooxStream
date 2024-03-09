3964ee9383d0eebf01ec57f3b834f2fbb09bea7a
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:46:10 +0700

    update Musikbot/utils/logger.py

--- Musikbot/utils/logger.py
+++ Musikbot/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import is_on_off
+from Musikbot import app
+from Musikbot.utils.database import is_on_off
 from config import LOGGER_ID
 
 
