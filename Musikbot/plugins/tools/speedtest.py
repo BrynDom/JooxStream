cc5968994a45689d426f255407c2d755c242ae72
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:47:37 +0700

    update Musikbot/plugins/tools/speedtest.py

--- Musikbot/plugins/tools/speedtest.py
+++ Musikbot/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.decorators.language import language
+from Musikbot import app
+from Musikbot.misc import SUDOERS
+from Musikbot.utils.decorators.language import language
 
 
 def testspeed(m, _):
