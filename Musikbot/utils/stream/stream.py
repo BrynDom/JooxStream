f35f14125cf0031ba1e4a868bb853408b66a1954
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:05:03 +0700

    update Musikbot/utils/stream/stream.py

--- Musikbot/utils/stream/stream.py
+++ Musikbot/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from DAXXMUSIC import Carbon, YouTube, app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.database import add_active_video_chat, is_active_chat
-from DAXXMUSIC.utils.exceptions import AssistantErr
-from DAXXMUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from DAXXMUSIC.utils.pastebin import DAXXBin
-from DAXXMUSIC.utils.stream.queue import put_queue, put_queue_index
-from DAXXMUSIC.utils.thumbnails import get_thumb
+from Musikbot import Carbon, YouTube, app
+from Musikbot.core.call import DAXX
+from Musikbot.misc import db
+from Musikbot.utils.database import add_active_video_chat, is_active_chat
+from Musikbot.utils.exceptions import AssistantErr
+from Musikbot.utils.inline import aq_markup, close_markup, stream_markup
+from Musikbot.utils.pastebin import DAXXBin
+from Musikbot.utils.stream.queue import put_queue, put_queue_index
+from Musikbot.utils.thumbnails import get_thumb
 
 
 async def stream(
