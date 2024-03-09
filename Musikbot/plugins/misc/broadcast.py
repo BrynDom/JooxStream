35762bce492349df1153e419c1049c8f9e63d777
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:30:48 +0700

    update Musikbot/plugins/misc/broadcast.py

--- Musikbot/plugins/misc/broadcast.py
+++ Musikbot/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from Musikbot import app
+from Musikbot.misc import SUDOERS
+from Musikbot.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from DAXXMUSIC.utils.decorators.language import language
-from DAXXMUSIC.utils.formatters import alpha_to_int
+from Musikbot.utils.decorators.language import language
+from Musikbot.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
@@ -117,7 +117,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from DAXXMUSIC.core.userbot import assistants
+        from Musikbot.core.userbot import assistants
 
         for num in assistants:
             sent = 0
