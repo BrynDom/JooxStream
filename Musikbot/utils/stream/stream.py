6d56ff25b6dd262ab4d9949196180665fbfebd71
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:04:15 +0700

    Add files via upload

--- Musikbot/utils/stream/stream.py
+++ Musikbot/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from Musikbot import Carbon, YouTube, app
-from Musikbot.core.call import Anony
-from Musikbot.misc import db
-from Musikbot.utils.database import add_active_video_chat, is_active_chat
-from Musikbot.utils.exceptions import AssistantErr
-from Musikbot.utils.inline import aq_markup, close_markup, stream_markup
-from Musikbot.utils.pastebin import AnonyBin
-from Musikbot.utils.stream.queue import put_queue, put_queue_index
-from Musikbot.utils.thumbnails import get_thumb
+from DAXXMUSIC import Carbon, YouTube, app
+from DAXXMUSIC.core.call import DAXX
+from DAXXMUSIC.misc import db
+from DAXXMUSIC.utils.database import add_active_video_chat, is_active_chat
+from DAXXMUSIC.utils.exceptions import AssistantErr
+from DAXXMUSIC.utils.inline import aq_markup, close_markup, stream_markup
+from DAXXMUSIC.utils.pastebin import DAXXBin
+from DAXXMUSIC.utils.stream.queue import put_queue, put_queue_index
+from DAXXMUSIC.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Anony.force_stop_stream(chat_id)
+        await DAXX.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Anony.join_call(
+                await DAXX.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await AnonyBin(msg)
+            link = await DAXXBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(
+            await DAXX.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(chat_id, original_chat_id, file_path, video=None)
+            await DAXX.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(chat_id, original_chat_id, file_path, video=status)
+            await DAXX.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Anony.join_call(
+            await DAXX.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Anony.join_call(
+            await DAXX.join_call(
                 chat_id,
                 original_chat_id,
                 link,
