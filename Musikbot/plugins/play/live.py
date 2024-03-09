0728d56ff5408e90339ddecdc805ae2d6d47dc70
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:35:45 +0700

    update Musikbot/plugins/play/live.py

--- Musikbot/plugins/play/live.py
+++ Musikbot/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 import random
-from DAXXMUSIC import YouTube, app
-from DAXXMUSIC.utils.channelplay import get_channeplayCB
-from DAXXMUSIC.utils.decorators.language import languageCB
-from DAXXMUSIC.utils.stream.stream import stream
+from Musikbot import YouTube, app
+from Musikbot.utils.channelplay import get_channeplayCB
+from Musikbot.utils.decorators.language import languageCB
+from Musikbot.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
