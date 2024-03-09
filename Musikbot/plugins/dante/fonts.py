6ac4cf6711b4acb28fba95e49b3179a491b5eacf
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:31:02 +0700

    update Musikbot/plugins/dante/fonts.py

--- Musikbot/plugins/dante/fonts.py
+++ Musikbot/plugins/dante/fonts.py
@@ -1,8 +1,8 @@
 from pyrogram import  filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DAXXMUSIC.utils.daxx_font import Fonts
-from DAXXMUSIC import app
+from Musikbot.utils.daxx_font import Fonts
+from Musikbot import app
 
 @app.on_message(filters.command(["font", "fonts"]))
 async def style_buttons(c, m, cb=False):
