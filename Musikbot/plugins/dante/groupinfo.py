cac9b61c84542bc9c24090bca800d99dc3b6de37
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:31:50 +0700

    update Musikbot/plugins/dante/groupinfo.py

--- Musikbot/plugins/dante/groupinfo.py
+++ Musikbot/plugins/dante/groupinfo.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from DAXXMUSIC import app
+from Musikbot import app
 
 @app.on_message(filters.command("groupinfo", prefixes="/"))
 async def get_group_status(_, message: Message):
