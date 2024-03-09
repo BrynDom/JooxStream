c49266429dd5087db015c668b95a65ae2b5133dd
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:51:12 +0700

    update Musikbot/utils/inline/play.py

--- Musikbot/utils/inline/play.py
+++ Musikbot/utils/inline/play.py
@@ -1,8 +1,8 @@
 import math
 from pyrogram.types import InlineKeyboardButton
-from DAXXMUSIC import app
+from Musikbot import app
 import config
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from Musikbot.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
