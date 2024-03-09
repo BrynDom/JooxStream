dedda8ef33507922f8210909a0cca11a086a6b0c
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:28:33 +0700

    update Musikbot/plugins/dante/dicegame.py

--- Musikbot/plugins/dante/dicegame.py
+++ Musikbot/plugins/dante/dicegame.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, enums, filters
 #from config import *
 import asyncio
-from DAXXMUSIC import app as app
+from Musikbot import app as app
 
 from pyrogram.handlers import MessageHandler
 
