45350d47b9d048872d0fa68ebc510012a5725cf7
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:24:55 +0700

    update Musikbot/plugins/misc/mongochk.py

--- Musikbot/plugins/misc/mongochk.py
+++ Musikbot/plugins/misc/mongochk.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
 import re
-from DAXXMUSIC import app as DAXX
+from Musikbot import app as DAXX
 
 
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
