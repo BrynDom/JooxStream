13ad7a818a587eb6a64214f343605549c88d278e
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:22:52 +0700

    update Musikbot/plugins/misc/bored.py

--- Musikbot/plugins/misc/bored.py
+++ Musikbot/plugins/misc/bored.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from DAXXMUSIC import app
+from Musikbot import app
 
 # URL for the Bored API
 bored_api_url = "https://apis.scrimba.com/bored/api/activity"
