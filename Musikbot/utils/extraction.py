37f5d24cb21423de83315d9e54fcc327ac411d00
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:45:04 +0700

    update Musikbot/utils/extraction.py

--- Musikbot/utils/extraction.py
+++ Musikbot/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from DAXXMUSIC import app
+from Musikbot import app
 
 
 async def extract_user(m: Message) -> User:
