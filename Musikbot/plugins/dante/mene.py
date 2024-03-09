dee21f74e93bf10ac480ec363799ee836f0c008b
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:34:07 +0700

    update Musikbot/plugins/dante/mene.py

--- Musikbot/plugins/dante/mene.py
+++ Musikbot/plugins/dante/mene.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from DAXXMUSIC import app 
+from Musikbot import app 
 
 # Define a command handler for the /meme command
 @app.on_message(filters.command("meme"))
