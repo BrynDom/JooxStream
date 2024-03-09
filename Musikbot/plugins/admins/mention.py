1e6b07082dc4cb7b182168a1deff486efa15cc09
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:38:57 +0700

    update Musikbot/plugins/admins/mention.py

--- Musikbot/plugins/admins/mention.py
+++ Musikbot/plugins/admins/mention.py
@@ -1,8 +1,8 @@
 import asyncio
 from pyrogram.enums import ChatType, ChatMemberStatus
-from DAXXMUSIC import app
+from Musikbot import app
 from pyrogram import filters
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from Musikbot.utils.daxx_ban import admin_filter
 
 
 
