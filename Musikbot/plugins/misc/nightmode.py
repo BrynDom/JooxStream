b7b6249729404c938992d412fc269acdb3bf28fe
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:26:11 +0700

    update Musikbot/plugins/misc/nightmode.py

--- Musikbot/plugins/misc/nightmode.py
+++ Musikbot/plugins/misc/nightmode.py
@@ -1,10 +1,10 @@
 import random 
 from pyrogram import filters,Client,enums
-from DAXXMUSIC import app
+from Musikbot import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram.types import ChatPermissions
-from DAXXMUSIC.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
+from Musikbot.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
 
 
 
