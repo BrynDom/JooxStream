9e94a6b7d6de0de45d2e40d27c80a955cf8746cd
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:12:57 +0700

    update Musikbot/plugins/tools/gpdata.py

--- Musikbot/plugins/tools/gpdata.py
+++ Musikbot/plugins/tools/gpdata.py
@@ -1,10 +1,10 @@
 from pyrogram import enums
 from pyrogram.enums import ChatType
 from pyrogram import filters, Client
-from DAXXMUSIC import app
+from Musikbot import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from Musikbot.utils.daxx_ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 
