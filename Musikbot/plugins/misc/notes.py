2b977b4d068aa7e1103426b2ebbcc320b289d8d5
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:26:29 +0700

    update Musikbot/plugins/misc/notes.py

--- Musikbot/plugins/misc/notes.py
+++ Musikbot/plugins/misc/notes.py
@@ -1,10 +1,10 @@
-from DAXXMUSIC import app
+from Musikbot import app
 from config import BOT_USERNAME
 from pyrogram import filters
-from DAXXMUSIC.utils.daxx_ban import admin_filter
-from DAXXMUSIC.mongo.notesdb import *
-from DAXXMUSIC.utils.notes_func import GetNoteMessage, exceNoteMessageSender, privateNote_and_admin_checker
-from DAXXMUSIC.utils.yumidb import user_admin
+from Musikbot.utils.daxx_ban import admin_filter
+from Musikbot.mongo.notesdb import *
+from Musikbot.utils.notes_func import GetNoteMessage, exceNoteMessageSender, privateNote_and_admin_checker
+from Musikbot.utils.yumidb import user_admin
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , Message , CallbackQuery
 from pyrogram.enums import ChatMemberStatus
 
@@ -82,7 +82,7 @@ async def PrivateNote(client, message):
     else:
         if await is_pnote_on(chat_id):
             await message.reply(
-                "Your notes are currently being sent in private. DAXXMUSIC will send a small note with a button which redirects to a private chat.",
+                "Your notes are currently being sent in private. Musikbot will send a small note with a button which redirects to a private chat.",
                 quote=True
             )
         else:
