9475119614875efcbda4556c93d24c4dff5a8294
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:40:41 +0700

    update Musikbot/plugins/admins/totalmembers.py

--- Musikbot/plugins/admins/totalmembers.py
+++ Musikbot/plugins/admins/totalmembers.py
@@ -1,8 +1,8 @@
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from Musikbot.utils.daxx_ban import admin_filter
 import os
 import csv
 from pyrogram import Client, filters
-from DAXXMUSIC import app
+from Musikbot import app
 
 @app.on_message(filters.command("user") & admin_filter)
 def user_command(client, message):
