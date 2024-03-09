98dffe8fdeaff9bc09e99a1bdd1b59f71725e473
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:27:06 +0700

    update Musikbot/plugins/misc/truth_dare.py

--- Musikbot/plugins/misc/truth_dare.py
+++ Musikbot/plugins/misc/truth_dare.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 import requests
 import random
-from DAXXMUSIC import app
+from Musikbot import app
 
 # Truth or Dare API URLs
 truth_api_url = "https://api.truthordarebot.xyz/v1/truth"
