32ed32312cc2779686881b701531ce0d60d7783a
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:25:44 +0700

    update Musikbot/plugins/admins/auth.py

--- Musikbot/plugins/admins/auth.py
+++ Musikbot/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils import extract_user, int_to_alpha
-from DAXXMUSIC.utils.database import (
+from Musikbot import app
+from Musikbot.utils import extract_user, int_to_alpha
+from Musikbot.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from DAXXMUSIC.utils.decorators import AdminActual, language
-from DAXXMUSIC.utils.inline import close_markup
+from Musikbot.utils.decorators import AdminActual, language
+from Musikbot.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
