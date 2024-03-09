632b359711d00224d18fd1764c401d663396b25c
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:53:43 +0700

    update Musikbot/utils/decorators/admins.py

--- Musikbot/utils/decorators/admins.py
+++ Musikbot/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS, db
-from DAXXMUSIC.utils.database import (
+from Musikbot import app
+from Musikbot.misc import SUDOERS, db
+from Musikbot.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
