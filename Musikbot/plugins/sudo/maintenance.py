3bc6c89b3f833aaf6898781c66b48624778e9a04
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:38:49 +0700

    update Musikbot/plugins/sudo/maintenance.py

--- Musikbot/plugins/sudo/maintenance.py
+++ Musikbot/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from Musikbot import app
+from Musikbot.misc import SUDOERS
+from Musikbot.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
