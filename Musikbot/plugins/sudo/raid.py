bbc201385bab720029d427cf6b01f9c1f8a78d60
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:39:16 +0700

    update Musikbot/plugins/sudo/raid.py

--- Musikbot/plugins/sudo/raid.py
+++ Musikbot/plugins/sudo/raid.py
@@ -2,8 +2,8 @@ import pyrogram
 import time
 from pyrogram import filters
 from pyrogram import Client
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
+from Musikbot import app
+from Musikbot.misc import SUDOERS
 
 # Define the spam command handler
 @app.on_message(filters.command("raid", prefixes=".") & SUDOERS)
