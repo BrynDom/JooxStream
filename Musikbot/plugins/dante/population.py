aff8118ae13bac8ec6de7c34750b345f28a71c8c
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:34:32 +0700

    update Musikbot/plugins/dante/population.py

--- Musikbot/plugins/dante/population.py
+++ Musikbot/plugins/dante/population.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
-from DAXXMUSIC import app
+from Musikbot import app
 
 
 @app.on_message(filters.command("population"))
