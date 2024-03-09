c6af076ac7b2d61de8c57a74489b3f8685542134
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:19:31 +0700

    update Musikbot/plugins/tools/webshot.py

--- Musikbot/plugins/tools/webshot.py
+++ Musikbot/plugins/tools/webshot.py
@@ -5,7 +5,7 @@ import requests
 from aiohttp import ClientSession
 from pyrogram import filters
 from pyrogram.types import *
-from DAXXMUSIC import app
+from Musikbot import app
 
 button = InlineKeyboardMarkup([[
             InlineKeyboardButton("⌯ ᴄʟᴏsᴇ ⌯", callback_data="close_data")
