d018f1aa9b1a80d09714f049bb6743c60adfc21e
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:51:43 +0700

    update Musikbot/utils/inline/queue.py

--- Musikbot/utils/inline/queue.py
+++ Musikbot/utils/inline/queue.py
@@ -1,6 +1,6 @@
 from typing import Union
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from Musikbot import app
+from Musikbot.utils.formatters import time_to_seconds
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
