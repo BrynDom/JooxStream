d557a9cba257d0a175778ece3b47a2059adaf27c
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 12:37:18 +0700

    update Musikbot/plugins/bot/botschk.py

--- Musikbot/plugins/bot/botschk.py
+++ Musikbot/plugins/bot/botschk.py
@@ -1,14 +1,14 @@
 import random
 from pyrogram import filters
-from DAXXMUSIC import app
-from DAXXMUSIC import *
+from Musikbot import app
+from Musikbot import *
 from ... import *
 import config
 
 from ...logging import LOGGER
 
-from DAXXMUSIC import app, userbot
-from DAXXMUSIC.core.userbot import *
+from Musikbot import app, userbot
+from Musikbot.core.userbot import *
 
 import asyncio
 
@@ -19,7 +19,7 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from dotenv import load_dotenv
-from DAXXMUSIC.core.userbot import Userbot
+from Musikbot.core.userbot import Userbot
 from datetime import datetime
 
 # Assuming Userbot is defined elsewhere
