7849566799c270390474d25210519a8fd2dc405f
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:35:54 +0700

    update Musikbot/plugins/play/channel.py

--- Musikbot/plugins/play/channel.py
+++ Musikbot/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import set_cmode
-from DAXXMUSIC.utils.decorators.admins import AdminActual
+from Musikbot import app
+from Musikbot.utils.database import set_cmode
+from Musikbot.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
