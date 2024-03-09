471520132b233ee50a4c7298672321799ffae9f2
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-09 14:25:57 +0700

    update Musikbot/plugins/admins/callback.py

--- Musikbot/plugins/admins/callback.py
+++ Musikbot/plugins/admins/callback.py
@@ -3,10 +3,10 @@ from telegram import CallbackQuery
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DAXXMUSIC import YouTube, app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.misc import SUDOERS, db
-from DAXXMUSIC.utils.database import (
+from Musikbot import YouTube, app
+from Musikbot.core.call import DAXX
+from Musikbot.misc import SUDOERS, db
+from Musikbot.utils.database import (
     get_active_chats,
     get_lang,
     get_upvote_count,
@@ -23,12 +23,12 @@ from pyrogram.errors import (
     UserAlreadyParticipant,
     UserNotParticipant,
 )
-from DAXXMUSIC.utils.database import get_assistant
-from DAXXMUSIC.utils.decorators.language import languageCB
-from DAXXMUSIC.utils.formatters import seconds_to_min
-from DAXXMUSIC.utils.inline import close_markup, stream_markup, stream_markup_timer
-from DAXXMUSIC.utils.stream.autoclear import auto_clean
-from DAXXMUSIC.utils.thumbnails import get_thumb
+from Musikbot.utils.database import get_assistant
+from Musikbot.utils.decorators.language import languageCB
+from Musikbot.utils.formatters import seconds_to_min
+from Musikbot.utils.inline import close_markup, stream_markup, stream_markup_timer
+from Musikbot.utils.stream.autoclear import auto_clean
+from Musikbot.utils.thumbnails import get_thumb
 from config import (
     BANNED_USERS,
     SOUNCLOUD_IMG_URL,
