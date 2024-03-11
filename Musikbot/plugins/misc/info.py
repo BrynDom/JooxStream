21a4a83f19290c70a3ab96757d16155bef3f10e0
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-12 01:41:07 +0700

    update Musikbot/plugins/misc/info.py

--- Musikbot/plugins/misc/info.py
+++ Musikbot/plugins/misc/info.py
@@ -10,11 +10,11 @@ from typing import Union, Optional
 import random
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://mallucampaign.in/images/img_1710090490.jpg",
+    "https://mallucampaign.in/images/img_1710090490.jpg",
+    "https://mallucampaign.in/images/img_1710090490.jpg",
+    "https://mallucampaign.in/images/img_1710090490.jpg",
+    "https://mallucampaign.in/images/img_1710090490.jpg",
 ]
 
 # --------------------------------------------------------------------------------- #
