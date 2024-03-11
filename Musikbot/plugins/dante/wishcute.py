290d88217049deedf5a473810e662137c8afa13b
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-12 01:33:07 +0700

    update Musikbot/plugins/dante/wishcute.py

--- Musikbot/plugins/dante/wishcute.py
+++ Musikbot/plugins/dante/wishcute.py
@@ -9,27 +9,27 @@ SUPPORT_CHAT = "MusicStreamSupport"
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
     if len(m.command) < 2:
-        await m.reply("ᴀᴅᴅ ᴡɪꜱʜ!")
+        await m.reply("Tambahkan keinginan")
         return 
 
     api = requests.get("https://nekos.best/api/v2/happy").json()
     url = api["results"][0]['url']
     text = m.text.split(None, 1)[1]
     wish_count = random.randint(1, 100)
-    wish = f"ʜᴇʏ! {m.from_user.first_name}! "
-    wish += f"ʏᴏᴜʀ ᴡɪꜱʜ: {text} "
-    wish += f"ᴘᴏꜱꜱɪʙʟᴇ ᴛᴏ: {wish_count}%"
+    wish = f"Hallo! {m.from_user.first_name}! "
+    wish += f"Keinginan Anda {text} "
+    wish += f"Mungkin untuk {wish_count}%"
     
     await app.send_animation(
         chat_id=m.chat.id,
         animation=url,
         caption=wish,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}")]])
+            [[InlineKeyboardButton("Support", url=f"https://t.me/{SUPPORT_CHAT}")]])
     )
             
     
-BUTTON = [[InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}")]]
+BUTTON = [[InlineKeyboardButton("Support", url=f"https://t.me/{SUPPORT_CHAT}")]]
 CUTIE = "https://64.media.tumblr.com/d701f53eb5681e87a957a547980371d2/tumblr_nbjmdrQyje1qa94xto1_500.gif"
 
 @app.on_message(filters.command("cute"))
@@ -43,7 +43,7 @@ async def cute(_, message):
 
     mention = f"[{user_name}](tg://user?id={str(user_id)})"
     mm = random.randint(1, 100)
-    CUTE = f" {mention} {mm}% ᴄᴜᴛᴇ ʙᴀʙʏ"
+    CUTE = f" {mention} {mm}% Bayi lucu"
 
     await app.send_document(
         chat_id=message.chat.id,
