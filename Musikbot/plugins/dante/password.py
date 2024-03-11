c16bd8445cb52ec2a298bab0910b6cce25802d3d
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-03-12 01:27:43 +0700

    update Musikbot/plugins/dante/password.py

--- Musikbot/plugins/dante/password.py
+++ Musikbot/plugins/dante/password.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @app.on_message(filters.command(["genpassword", 'genpw']))
 async def password(bot, update):
-    message = await update.reply_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ..")
+    message = await update.reply_text(text="Prosess..")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
     if len(update.command) > 1:
         qw = update.text.split(" ", 1)[1]
@@ -15,6 +15,6 @@ async def password(bot, update):
         qw = random.choice(ST)
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
-    txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/YumikooBot?startgroup=true')]])
+    txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Add Me', url='https://t.me/StreamingManageBot?startgroup=true')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
