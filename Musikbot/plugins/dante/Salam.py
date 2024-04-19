d0d2b7e3d2c6aa7b1d676adf7a2b6569f6599ffe
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-04-20 00:21:42 +0700

    update Musikbot/plugins/dante/Salam.py

--- Musikbot/plugins/dante/Salam.py
+++ Musikbot/plugins/dante/Salam.py
@@ -1,79 +1,53 @@
 
-"""
-Perintah Tersedia
 
-• `{i}ass`
-   Salam 
-
-• `{i}as`
-   Assalamu'alaikum
-
-• `{i}ws`
-   Jawab Salam
-   
-• `{i}ks`
-   Kenalan 
-   
-• `{i}jws`
-   Istighfar Salam
-   
-• `{i}3x`
-    Bisa 
-
-• `{i}kg`
-    Keren lu
-
-• `{i}hm`
-    Batuk
-"""
 
 from time import sleep
 from . import (
     eor,
-    dante_cmd,
+    app,
 )
 
-@dante_cmd(pattern="ass$")
+@app.on_message(pattern="ass$")
 async def _(event):
     await event.eor("**Assalamu'alaikum Warohmatulohi Wabarokatu**")
 
 
-@dante_cmd(pattern="as$")
+@app.on_message(pattern="as$")
 async def _(event):
     await event.eor("**Assalamu'alaikum**")
     
-@dante_cmd(pattern="ws$")
+@app.on_message(pattern="ws$")
 async def _(event):
     await event.eor("**Wa'alaikumussalam**")
 
     
-@dante_cmd(pattern="ks$")
+@app.on_message(pattern="ks$")
 async def _(event):
     xx = await event.eor("**Hy kaa 🥹**")
     sleep(2)
     await xx.edit("**Assalamualaikum...**")
 
 
-@dante_cmd(pattern="jws$")
+@app.on_message(pattern="jws$")
 async def _(event):
     xx = await event.eor(event, "**Astaghfirullah, Jawab dulu salam dong**")
     sleep(2)
     await xx.edit("**Assalamu'alaikum**")
 
 
-@dante_cmd(pattern="3x$")
+@app.on_message(pattern="3x$")
 async def _(event):
     xx = await event.eor("**Bismillah, 3x**")
     sleep(2)
     await xx.edit("**Assalamu'alaikum Bisa yug Kali**")
     
-@dante_cmd(pattern="kg$")
+@app.on_message(pattern="kg$")
 async def _(event):
     xx = await event.eor("**Nih Gw Pantun,Buah Apel Buah Kedondong**")
     sleep(2)
     await xx.edit("**Senggol Dong!!!**")
 
-@dante_cmd(pattern="hm$")
+@app.on_message(pattern="hm$")
 async def _(event):
     xx = await event.eor("**Batuk dulu g sih**")
     sleep(2)
