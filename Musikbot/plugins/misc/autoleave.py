6e987f6b364e35dae189d5a5a1d7163098d80365
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-30 20:11:01 +0700

    Update autoleave.py

--- Musikbot/plugins/misc/autoleave.py
+++ Musikbot/plugins/misc/autoleave.py
@@ -30,8 +30,8 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOGGER_ID
-                                and i.chat.id != -1002009684047
-                                and i.chat.id != -1002009684047
+                                and i.chat.id != -1002107353194
+                                and i.chat.id != -1002107353194
                             ):
                                 if left == 20:
                                     continue
