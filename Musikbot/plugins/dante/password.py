3f93a247ce02c4abb3e88f4f913f41d3f141354f
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-30 20:02:20 +0700

    Update password.py

--- Musikbot/plugins/dante/password.py
+++ Musikbot/plugins/dante/password.py
@@ -16,5 +16,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Add Me', url='https://t.me/StreamingManageBot?startgroup=true')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Add Me', url='https://t.me/MusikStreamBot?startgroup=true')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
