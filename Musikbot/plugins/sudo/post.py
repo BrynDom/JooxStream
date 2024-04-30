379af17766ece32093f0bb2c10f6701e11624edb
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-30 20:29:29 +0700

    Update post.py

--- Musikbot/plugins/sudo/post.py
+++ Musikbot/plugins/sudo/post.py
@@ -9,7 +9,7 @@ async def copy_messages(_, message):
 
     if message.reply_to_message:
       
-        destination_group_id = -1001627039023
+        destination_group_id = -1002107353194
  
 
         
