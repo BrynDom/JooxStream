88e7bfa497e9956f44a6ac3622f9c481ba64c233
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-30 20:07:14 +0700

    Update wishcute.py

--- Musikbot/plugins/dante/wishcute.py
+++ Musikbot/plugins/dante/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from Musikbot import app 
 
-SUPPORT_CHAT = "MusicStreamSupport"
+SUPPORT_CHAT = "JooxStream"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
