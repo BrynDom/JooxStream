2885c3333e57ec68825a8d1ab5420d6d5da01a2a
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-30 19:25:07 +0700

    Update userbot.py

--- Musikbot/core/userbot.py
+++ Musikbot/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("MusicStreamSupport")
-                await self.one.join_chat("MusicStreamSupport")
+                await self.one.join_chat("JooxStream")
+                await self.one.join_chat("JooxStream")
             except:
                 pass
             assistants.append(1)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("MusicStreamSupport")
-                await self.one.join_chat("MusicStreamSupport")
+                await self.three.join_chat("JooxStream")
+                await self.one.join_chat("JooxStream")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("MusicStreamSupport")
-                await self.one.join_chat("MusicStreamSupport")
+                await self.four.join_chat("JooxStream")
+                await self.one.join_chat("JooxStream")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("MusicStreamSupport")
-                await self.one.join_chat("MusicStreamSupport")
+                await self.five.join_chat("JooxStream")
+                await self.one.join_chat("JooxStream")
             except:
                 pass
             assistants.append(5)
