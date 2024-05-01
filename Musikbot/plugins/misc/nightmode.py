91ce871d43460d74a0ba40e05025ff1aff5045d0
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-05-01 17:31:05 +0700

    Update nightmode.py

--- Musikbot/plugins/misc/nightmode.py
+++ Musikbot/plugins/misc/nightmode.py
@@ -5,31 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram.types import ChatPermissions
 from Musikbot.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
-
-
-
-CLOSE_CHAT = ChatPermissions(
-    can_send_messages=False,
-    can_send_media_messages = False,
-    can_send_other_messages = False,
-    can_send_polls = False,
-    can_change_info = False,
-    can_add_web_page_previews = False,
-    can_pin_messages = False,
-    can_invite_users = False )
-
-
-OPEN_CHAT = ChatPermissions(
-    can_send_messages=True,
-    can_send_media_messages = True,
-    can_send_other_messages = True,
-    can_send_polls = True,
-    can_change_info = True,
-    can_add_web_page_previews = True,
-    can_pin_messages = True,
-    can_invite_users = True )
-    
-buttons = InlineKeyboardMarkup([[InlineKeyboardButton("Enable", callback_data="add_night"),InlineKeyboardButton("Disable", callback_data="rm_night")]])         
+       
 
 @app.on_message(filters.command("nightmode") & filters.group)
 async def _nightmode(_, message):
