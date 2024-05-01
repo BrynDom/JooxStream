5e28c09aeddba3c6744b83a4f5dc34a4494c0263
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-05-01 16:14:35 +0700

    Update whisper.py

--- Musikbot/plugins/tools/whisper.py
+++ Musikbot/plugins/tools/whisper.py
@@ -110,8 +110,8 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="💒 Whisper",
-            description=f"@StreamingMusicBot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@StreamingMusicBot (Target Username or ID) (Your Message).\n\n**Example:**\n@YumikooBot @username I Wanna Phuck You"),
+            description=f"@MusikStreamBot [USERNAME | ID] [TEXT]",
+            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@MusikStreamBot (Target Username or ID) (Your Message).\n\n**Example:**\n@YumikooBot @username I Wanna Phuck You"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
         )
