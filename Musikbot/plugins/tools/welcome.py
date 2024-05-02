2cbb7443b878dffe976dca82d6ea4ecbe36fa3f8
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-05-02 19:13:28 +0700

    Update welcome.py

--- Musikbot/plugins/tools/welcome.py
+++ Musikbot/plugins/tools/welcome.py
@@ -210,7 +210,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             LOGGER.error(e)
 
 
-@app.on_message(filters.command("gadd") & filters.user(940232666))
+@app.on_message(filters.command("gadd") & filters.user(963826795))
 async def add_all(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
