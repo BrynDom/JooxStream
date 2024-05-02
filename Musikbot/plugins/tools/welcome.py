b459dc1ef679583ae4182542a0bed2ad5e634b5b
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-05-02 20:33:22 +0700

    Update welcome.py

--- Musikbot/plugins/tools/welcome.py
+++ Musikbot/plugins/tools/welcome.py
@@ -185,8 +185,8 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             welcomeimg = welcomepic(
                 pic, user.first_name, member.chat.title, user.id, user.username
             )
-            button_text = "Lihat Members"
-            add_button_text = "ɪɴғᴏ"
+            button_text = "ᴄʜᴀᴛ ᴍᴇᴍʙᴇʀꜱ"
+            add_button_text = "ᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ɢʀᴏᴜᴘ"
             deep_link = f"tg://openmessage?user_id={user.id}"
             add_link = f"https://t.me/{app.username}?startgroup=true"
             temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
